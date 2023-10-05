48847b00c85b3d4175bcb3ee5c898b4fe09d534e
Alexis <alexisperez4@gmail.com>
2023-10-05 02:55:57 +0000

    add CardCharacter component

--- src/components/listCharacters.vue
+++ src/components/listCharacters.vue
@@ -1,8 +1,8 @@
 <template>
     <section>
         <div class="characters">
-            <div class="characters__item">
-                <h2>Characteres</h2>
+            <div class="characters__item" v-for="character in characters" :key="character.id">
+                <CardCharacter :character = character />
             </div>
 
         </div>
@@ -13,17 +13,39 @@
 
 <script>
 
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { useStore } from 'vuex'
+import CardCharacter from './CardCharacter.vue'
+
 
 export default {
+    components: {
+        CardCharacter
+    },
     setup(){
         const store = useStore()
+        const characters = computed(()=>{
+            return store.state.characterFilter
+        })
 
         onMounted(()=>{
             store.dispatch('getCharacters')
         })
+
+        return {
+            characters
+        }
     }
 }
 
 </script>
+
+
+<style>
+.characters {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    margin: 3rem 0;
+}
+</style>
