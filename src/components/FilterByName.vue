c5cfe72c5e77fdd99df48d1bb27ad9ad3caf6512
Alexis <alexisperez4@gmail.com>
2023-10-05 14:03:55 +0000

    add filter by name component

---
+++ src/components/FilterByName.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search">
+        <input 
+            type="text"
+            placeholder="Search by name"
+            v-model="name"
+            @keyup="filter()"
+        >
+    </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+import { ref } from 'vue'
+export default {
+    setup() {
+        const store = useStore()
+        const name = ref('')
+
+        const filter = () => {
+            store.dispatch('filterByName', name.value)
+        }
+
+        return {
+            name,
+            filter
+        }
+    }
+}
+</script>
+
